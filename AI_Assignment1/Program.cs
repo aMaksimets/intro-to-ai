e235613d1a4328b6eea94140a29f0621e8e08d68
blkhood <102079222@student.swin.edu.au>
2020-04-14 21:51:25 +1000

    new program in csharp

---
+++ AI_Assignment1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace AI_Assignment1
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileStatus = (args.Length >= 2) ? "Loading!" : "Syntax: <AI_Assignment1> <file> <bfs|dfs>";
+            Console.WriteLine(fileStatus);
+
+            string Algo = args[1];
+            _ = args[0] + ".txt";
+
+            // case swap for Alogrithm
+        }
+    }
+}
